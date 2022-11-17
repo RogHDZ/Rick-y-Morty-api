2296fda08b76c466071591fba9732a54985535c1
rogeliohdz <rogeliohernandezserafin>
2022-11-17 10:22:05 -0600

    commit final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { modalBody, spinner, creaCard } from './ui.js'
+
 const urlBase = 'https://rickandmortyapi.com/api/character/';
 
 const loadData = (urlBase, page = 1) => {
@@ -44,23 +46,7 @@ const loadData = (urlBase, page = 1) => {
     }
 }
 
-const creaCard = (personaje) => {
-    const html = `
-    <div class="card bg-dark text-ligth border" style="width: 18rem;">
-        <img src="${personaje.image}" class="card-img-top" alt="${personaje.image}">
-        <div class="card-body">
-            <h5 class="card-title">${personaje.name}</h5>
-            <p class="card-text">${personaje.status}</p>
-            <a href="#" 
-            class="btn btn-primary" 
-            data-bs-toggle="modal" 
-            data-bs-target="#exampleModal"
-            data-id="${personaje.id}">Ver mas</a>
-        </div>
-    </div>
-`;
-    return html;
-}
+
 
 const navegacion = (e) => {
     e.preventDefault();
@@ -84,17 +70,7 @@ const showCharacterById = (id) => {
         })
 }
 
-const modalBody = (personaje) => {
-    const div = document.createElement('div');
-    div.classList.add('text-center');
-    let html = ``;
-    html += `<img src="${personaje.image}">`;
-    html += `<p>${personaje.status} - ${personaje.species}</p>`;
-    html += `<p>ultima ubicacion conocida</p><p>${personaje.origin.name}</p>`;
-    html += `<p>Ha aparecido en ${personaje.episode.length} Episodios</p>`;
-    div.innerHTML = html;
-    return div;
-}
+
 
 const loadInfo = (e) => {
     e.preventDefault();
@@ -112,17 +88,7 @@ const loadInfo = (e) => {
     }
 }
 
-const spinner = () => {
-    const div = document.createElement('div');
-    const html = 
-    `<div class="d-flex justify-content-center">
-    <div class="spinner-border text-info" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  </div>`;
-  div.innerHTML = html;
-  return div;
-}
+
 document.querySelector('#botones').addEventListener('click', navegacion);
 document.querySelector('#characters').addEventListener('click', loadInfo);
 loadData(urlBase);
